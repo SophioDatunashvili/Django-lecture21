a67570834c9c048cc5895eb4a9cdadf35ad55838
SophioDatunashvili <sophio.datunashvili@iset.ge>
2023-01-14 20:12:07 +0400

    create lecturer and subject

--- lecture/models.py
+++ lecture/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+
+class Lecturer(models.Model):
+    name = models.CharField(max_length=50)
+    surname = models.CharField(max_length=150)
+    picture = models.ImageField(upload_to="lecturer/", blank=True, null=True)
+
+
+    def __str__(self):
+        return self.surname
+
+
+class Subject(models.Model):
+    name = models.CharField(max_length=150)
+    start_date = models.DateField()
+    duration = models.CharField(max_length=150)
+    lecturer = models.ForeignKey(Lecturer, on_delete=models.CASCADE, related_name="subjects")
+
+
+    def __str__(self):
+        self.name
+
+
